0faa4d03aec99001f511e29c5c3f002fab094286
Sanket Bisne <sanketbisne@Sankets-MacBook-Air.local>
2024-06-14 01:52:27 +0530

    Add working code

--- gcs_buckets/main.tf
+++ gcs_buckets/main.tf
@@ -1,18 +1,23 @@
-resource "google_storage_bucket" "example" {
+resource "google_storage_bucket" "buckets" {
   for_each = var.buckets
 
-  name         = each.value.name
-  location     = each.value.location
-  storage_class = each.value.storage_class
+  name                        = each.key
+  location                    = var.region
+  project                     = var.project
+  storage_class               = each.value.storage_class
+  public_access_prevention    = each.value.public_access_prevention
+  uniform_bucket_level_access = each.value.uniform_bucket_level_access
 
-  default_event_based_hold    = false
-  enable_object_retention     = false
-  force_destroy               = false
-  public_access_prevention    = "enforced"
-  requester_pays              = false
-  uniform_bucket_level_access = true
-
-  soft_delete_policy {
-    # Define your soft delete policy here if needed
+  dynamic "lifecycle_rule" {
+    for_each = each.value.lifecycle_rule
+    content {
+      condition {
+        age = lifecycle_rule.value.age
+      }
+      action {
+        type = lifecycle_rule.value.type
+      }
+    }
   }
 }
+
--- gcs_buckets/variables.tf
+++ gcs_buckets/variables.tf
@@ -1,8 +1,23 @@
+variable "project" {
+  description = "The Google Cloud project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for the Google Cloud resources"
+  type        = string
+}
+
 variable "buckets" {
-  description = "Map of bucket names to their configurations."
-  type = map(object({
-    name = string
-    location      = string
-    storage_class = string
+  description = "Map of Google Cloud Storage buckets to create"
+  type        = map(object({
+    storage_class               = string
+    public_access_prevention    = string
+    uniform_bucket_level_access = bool
+    lifecycle_rule              = list(object({
+      age  = number
+      type = string
+    }))
   }))
+  default     = {}
 }

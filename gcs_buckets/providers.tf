dc75c500391230379d306cafd2989b6d1dea6726
sanketbisne <sanketbisne@gmail.com>
2024-06-15 10:26:37 +0530

    Add root and child modules

--- gcs_buckets/providers.tf
+++ gcs_buckets/providers.tf
@@ -1,4 +1,4 @@
 provider "google" {
-  project = "qwiklabs-gcp-04-0e17b33e2892"  # Update this with your project ID
+  project = "clgcporg8-076"  # Update this with your project ID
   region  = "ASIA-SOUTH1"
 }
